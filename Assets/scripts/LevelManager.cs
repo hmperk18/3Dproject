1c73b1ff0984ad8fb93d0e1cb63b915385240990
Haille <hmlperkins@hotmail.com>
2021-12-03 19:21:48 +0100

    work on UI

--- Assets/scripts/LevelManager.cs
+++ Assets/scripts/LevelManager.cs
@@ -1,17 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
-    public static LevelManager instance;
-    private static int maxRooms = 4;
-    [SerializeField] GameObject[] itemRooms = new GameObject[maxRooms]; // list of possible item spawn locations
+    static public LevelManager instance;
+    [SerializeField] GameObject player; // reference to the player
 
+    static private int maxRooms = 4;
+    [SerializeField] GameObject[] itemRooms = new GameObject[maxRooms]; // list of possible item spawn locations
     private int roomsIdx; // next free spot in the rooms list
+
+    static private int maxHearts = 3; // number of hits
+    private int currHearts; // current hearts
+    [SerializeField] Image[] lives = new Image[maxHearts]; // heart icons
+    [SerializeField] Sprite emptyHeart;
+
+    static public int winCondition = 3; // number of keys needed
+    private int currKeys; // current hearts
+    [SerializeField] Image[] keys = new Image[winCondition]; // key icons
+
 
     private void Awake()
     {
+        // init vars
+        currHearts = maxHearts;
         instance = this;
         roomsIdx = 0;
     }
@@ -24,9 +37,6 @@ public class LevelManager : MonoBehaviour
 
     private void _AddRoom(GameObject room)
     {
-        Debug.Log("spawned room");
-        Debug.Log(roomsIdx);
-        
         itemRooms[roomsIdx] = room;
         roomsIdx += 1; 
 
@@ -36,13 +46,67 @@ public class LevelManager : MonoBehaviour
         {
             // spawn some items in the last few item rooms
                 // took long to add to more rooms away from the center
-            for(int i = maxRooms - 1; i >= 1; i--)
+            for(int i = winCondition; i >= 1; i--)
             {
-                Debug.Log("spawn item");
                 itemRooms[i].GetComponent<ItemRoom>().SpawnItem();
             }
         }
     }
 
     //TODO: keys collected UI
+    
+    // player takes damage
+    public static void Damaged()
+    {
+        instance._Damaged();
+    }
+
+    // display win screen
+    public static void WinGame()
+    {
+        instance._WinGame();
+    }
+
+    // private method to increase score
+    private void _IncreaseScore()
+    {
+        currKeys += 1;
+        //scoreText.text = "Croissants: " + currKeys.ToString() + "/" + winCondition.ToString();
+        checkWin();
+    }
+
+    // remove heart icons from UI for each hit taken
+    private void _Damaged()
+    {
+        currHearts--;
+
+        // make heart an empty heart sprite after taking damage
+        lives[currHearts].sprite = emptyHeart;
+
+        // reload the game if at zero hearts
+        // TODO - pop up lost screen?
+            // return at hallway or new run options
+        if (currHearts == 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+    }
+
+    // check that the minimum score to win has been achieved
+    private void checkWin()
+    {
+        // open the door if score is enough
+        if (currKeys == winCondition)
+        {
+            // todo
+
+        }
+    }
+
+    // display a message to the winner about their score
+    private void _WinGame()
+    {
+        //todo
+    }
 }
